598528356fdd7c4ee9fc79734e236c7e76fd5fea
Sarajevo <dmgpacleet@gmail.com>
2025-09-21 17:52:53 -0300

    backligh detector for polybar better error handeling

--- .config/polybar/backlight-detector.sh
+++ .config/polybar/backlight-detector.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-# script-backlight.sh
-echo $(ls -1 /sys/class/backlight | head -n 1)
-
+#!/usr/bin/env sh
+backlight_device=$(ls -1 /sys/class/backlight 2>/dev/null | head -n 1)
+echo "${backlight_device:-intel_backlight}"

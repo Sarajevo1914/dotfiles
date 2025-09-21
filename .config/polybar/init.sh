2ddddc9a2d3b123a00bcba9c5f03e4384b368d96
Sarajevo <dmgpacleet@gmail.com>
2025-09-21 17:56:01 -0300

    polybar, adding multimonitor setup and improving init.sh

--- .config/polybar/init.sh
+++ .config/polybar/init.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-# polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+MONITORS=$(xrandr --listmonitors 2>/dev/null | grep -v "Monitors" | awk '{print $4}')
+
+if [ -z "$MONITORS" ]; then
+    echo "No monitors detected, exiting..."
+    exit 1
+fi
+
+MONITOR_ARRAY=($MONITORS)
+
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+if [ ${#MONITOR_ARRAY[@]} -ge 2 ]; then
+    MONITOR=${MONITOR_ARRAY[0]} polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+    MONITOR=${MONITOR_ARRAY[1]} polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+    echo "Launched polybar on monitors: ${MONITOR_ARRAY[0]} and ${MONITOR_ARRAY[1]}"
+else
+    MONITOR=${MONITOR_ARRAY[0]} polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+    echo "Launched polybar on monitor: ${MONITOR_ARRAY[0]}"
+fi
 
 echo "Bars launched..."
